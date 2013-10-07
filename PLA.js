f56afa5b309fc913e32d6cf3047195692fe96010
Xelio <xelio.cheong@gmail.com>
2013-10-07 08:42:13 +0800

    Refactoring: Move function of set training into PLA, simplified the code

--- PLA.js
+++ PLA.js
@@ -8,25 +8,49 @@ window.PLA = function(weights, modifier) {
   this._iteration = 0;
 }
 
-window.PLA.prototype.train = function(inputs, expectedOutput) {
-  if(expectedOutput === this.getOutput(inputs)) return false;
+window.PLA.prototype.train = function(data) {
+  if(data.output === this.getOutput(data.point)) return false;
 
-  this._weights[0] += expectedOutput;
-  for(var i = 0; i < inputs.length; i++) {
-    this._weights[i+1] += expectedOutput * inputs[i];
+  this._weights[0] += data.output;
+  for(var i = 0; i < data.point.length; i++) {
+    this._weights[i+1] += data.output * data.point[i];
   }
 
   this._iteration += 1;
   return true;
 }
 
-window.PLA.prototype.getOutput = function(inputs) {
-  if(inputs.length !== this._weights.length - 1) throw "Number of inputs is not correct"
+// iterationCallback(weightCurrent, weightsBefore, iteration, trainedPoint)
+// completeCallback(weight, iteration)
+window.PLA.prototype.trainSet = function(dataSet, singleIteration, iterationCallback, completeCallback) {
+  var misclassifiedDataSet;
+  do {
+    // Check training complete
+    misclassifiedDataSet = [];
+    for(var i in dataSet) {
+      if(dataSet[i].output !== this.getOutput(dataSet[i].point)) {
+        misclassifiedDataSet.push(dataSet[i]);
+      }
+    }
+
+    if(misclassifiedDataSet.length !== 0) {
+      var misclassifiedData = misclassifiedDataSet[Math.floor(Math.random() * misclassifiedDataSet.length)];
+      var weightsBefore = $.extend([],this._weights);
+      this.train(misclassifiedData);
+      if(iterationCallback) iterationCallback(this._weights, weightsBefore, this._iteration, misclassifiedData.point);
+    }
+  } while(misclassifiedDataSet.length !== 0 && !singleIteration)
+
+  if(misclassifiedDataSet.length === 0 && completeCallback) completeCallback(this._weights, this._iteration);
+}
+
+window.PLA.prototype.getOutput = function(point) {
+  if(point.length !== this._weights.length - 1) throw "Dimension of point is not correct"
 
   var output;
   output = this._weights[0];
-  for(var i = 0; i < inputs.length; i++) {
-    output += this._weights[i+1] * inputs[i];
+  for(var i = 0; i < point.length; i++) {
+    output += this._weights[i+1] * point[i];
   }
   
   if(typeof(this._modifier) === 'function') output = this._modifier(output);
@@ -41,13 +65,13 @@ window.TestPlane = function(x_min, x_max, y_min, y_max) {
   this._line = [this.randomPoint(), this.randomPoint()];
 }
 
-window.TestPlane.prototype.getOutput = function(input) {
+window.TestPlane.prototype.getOutput = function(point) {
   var output;
   var Ax = this._line[0][0];
   var Ay = this._line[0][1];
   var Bx = this._line[1][0];
   var By = this._line[1][1];
-  output = Math.sign((Bx-Ax) * (input[1]-Ay) - (By-Ay) * (input[0]-Ax));
+  output = Math.sign((Bx-Ax) * (point[1]-Ay) - (By-Ay) * (point[0]-Ax));
   return output;
 }
 
