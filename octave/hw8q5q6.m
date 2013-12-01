00e4e349adaeb48fbd54954e499f3655f39eb8ac
Xelio <xelio.cheong@gmail.com>
2013-12-01 18:23:15 +0800

    Add SVM related files

---
+++ octave/hw8q5q6.m
@@ -0,0 +1,58 @@
+train_data = load('features.train');
+test_data = load('features.test');
+
+% 1 versus 5
+Q = 2;
+Cs = [0.0001, 0.001, 0.01, 0.1, 1];
+
+
+x = train_data(:, [2, 3]);
+y = train_data(:, 1);
+
+X = [x(find(y==1), [1,2]); x(find(y==5), [1,2])];
+Y = [ones(size(find(y==1),1), 1); ones(size(find(y==5),1), 1) .* -1];
+
+x_out = test_data(:, [2, 3]);
+y_out = test_data(:, 1);
+
+X_out = [x_out(find(y_out==1), [1,2]); x_out(find(y_out==5), [1,2])];
+Y_out = [ones(size(find(y_out==1),1), 1); ones(size(find(y_out==5),1), 1) .* -1];
+
+fprintf ("Number of training data : %i\n", size(Y,1));
+fprintf ("Number of testing data : %i\n", size(Y_out,1));
+fprintf ("\n");
+fprintf ("Q = %i", Q);
+
+SVs = zeros (size(Cs, 2), 1);
+Ein = zeros (size(Cs, 2), 1);
+Eout = zeros (size(Cs, 2), 1);
+
+for i = 1 : size(Cs, 2)
+	C = Cs(i);
+    parm = sprintf('-q -s 0 -t 1 -g 1 -r 1 -h 0 -d %i -c %f', Q, C);
+
+    model = svmtrain(Y, X, parm);
+
+    SVs(i) = model.totalSV;
+
+	in_predict = svmpredict(Y, X, model, '-q');
+	E_in(i) = size(find(Y != in_predict),1)/size(Y,1);
+
+	out_predict = svmpredict(Y_out, X_out, model, '-q');
+	E_out(i) = size(find(Y_out != out_predict),1)/size(Y_out,1);
+endfor
+fprintf ("\n");
+
+for i = 1 : size(Cs, 2)
+	fprintf ("Number of SV of C = %f : %i\n", Cs(i), SVs(i));
+endfor
+fprintf ("\n");
+
+for i = 1 : size(Cs, 2)
+	fprintf ("Ein of C = %f: %f\n", Cs(i), E_in(i));
+endfor
+fprintf ("\n");
+
+for i = 1 : size(Cs, 2)
+	fprintf ("Eout of C = %f: %f\n", Cs(i), E_out(i));
+endfor
